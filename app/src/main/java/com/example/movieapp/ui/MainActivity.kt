a119940406a0ef3c4473bc3b8a1634648cc94c5e
Ishtiaque <tmdisht@hlbb.hongleong.com.bd>
2023-01-26 14:42:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/ui/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.movieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.movieapp.R
+import com.example.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupDrawerLayout()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
+    }
+
+    private fun setupDrawerLayout() {
+        binding.navView.setupWithNavController(navController)
+    }
+    fun openDrawer(){
+        binding.drawerLayout.openDrawer(GravityCompat.START)
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/ui/bookmark/BookmarkFragment.kt
@@ -0,0 +1,20 @@
+package com.example.movieapp.ui.bookmark
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+
+class BookmarkFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+    }
+}
